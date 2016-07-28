50854ae3f5d6d383f4ce53f1d67a40c84f201c5b
Thiago Nogueira <tnogueira@uolinc.com>
2016-07-28 14:08:52 +0100

    Adicionando v2 de recurso de Paciente

---
+++ src/main/java/ldevpp/webinar/ddd/patients/PatientV2Controller.java
@@ -0,0 +1,48 @@
+package ldevpp.webinar.ddd.patients;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityLinks;
+import org.springframework.hateoas.ExposesResourceFor;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.Resources;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(tags = "patients")
+@Controller
+@RestController
+@ExposesResourceFor(Patient.class)
+public class PatientV2Controller {
+
+    @Autowired
+    private PatientRepository repository;
+
+    @Autowired
+    private EntityLinks entityLinks;
+
+    @ApiOperation(value = "Lista todos os pacientes")
+    @RequestMapping(path = "/v2/patients", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public HttpEntity<Resources<Patient>> showPatients() {
+        Resources<Patient> resources = new Resources<Patient>(this.repository.findAll());
+        resources.add(this.entityLinks.linkToCollectionResource(Patient.class));
+        return new ResponseEntity<Resources<Patient>>(resources, HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "Consulta paciente por Id")
+    @RequestMapping(path = "/v2/patients/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public HttpEntity<Resource<Patient>> showPatient(@PathVariable Long id) {
+        Resource<Patient> resource = new Resource<Patient>(this.repository.findOne(id));
+        resource.add(this.entityLinks.linkToSingleResource(Patient.class, id));
+        return new ResponseEntity<Resource<Patient>>(resource, HttpStatus.OK);
+    }
+}
